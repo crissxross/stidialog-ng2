6cd164771d94508349749e020a24958c2c3bb2f6
Christine Wilks <crissxross@gmail.com>
2016-01-31 16:29:43 +0000

    Add scene link on home page

--- src/app/components/home/home.ts
+++ src/app/components/home/home.ts
@@ -1,11 +1,12 @@
 import {Component, OnInit} from 'angular2/core';
 import {FORM_DIRECTIVES} from 'angular2/common';
+import {RouterLink} from 'angular2/router';
 
 import {Api} from 'app/services/api/api';
 
 @Component({
     selector: 'home',
-    directives: [...FORM_DIRECTIVES],
+    directives: [...FORM_DIRECTIVES, RouterLink],
     pipes: [],
     styles: [require('./home.scss')],
     template: require('./home.html')
